c32c4a41e2cc7f4df9ea8ce91d5c2e171900f459
liu245933567 <245933567@qq.com>
2022-03-07 16:16:13 +0800

    chore: 还原代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import zhCN from 'antd/lib/locale/zh_CN';
 import { ConfigProvider } from 'antd';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home, QRcode } from '@/route';
 import '@/style/index.less';
 
 function App() {
   return (
     <ConfigProvider locale={zhCN}>
-      <HashRouter>
+      <BrowserRouter basename="/blog/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/qrcode" element={<QRcode />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </ConfigProvider>
   );
 }
