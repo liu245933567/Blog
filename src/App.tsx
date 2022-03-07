134387a8ac069e476d80318cb5ce8042ccca1f8f
liu245933567 <245933567@qq.com>
2022-03-07 11:19:25 +0800

    chore: 更改为 hash 路由模式

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import zhCN from 'antd/lib/locale/zh_CN';
 import { ConfigProvider } from 'antd';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home, QRcode } from '@/route';
 import '@/style/index.less';
 
 function App() {
   return (
     <ConfigProvider locale={zhCN}>
-      <BrowserRouter basename="/blog/">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/qrcode" element={<QRcode />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ConfigProvider>
   );
 }
