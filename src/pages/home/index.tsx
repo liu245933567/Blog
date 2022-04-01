728cd5ab8120e4c5c2e17e524027626b03f4800f
liu245933567 <245933567@qq.com>
2022-04-01 15:07:28 +0800

    chore: 修改首页内容

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { Statistic } from 'antd';
+import './index.less';
+
+const { Countdown } = Statistic;
+
+dayjs.extend(relativeTime);
 
 export default function Home() {
-  return <div>Homeddd</div>;
+  return (
+    <div className="home">
+      <div className="home__img" />
+      <div className="home__time">
+        <Countdown
+          title="距离放假"
+          value="2022-04-02 18:30"
+          format="D 天 H 时 m 分 s 秒"
+        />
+      </div>
+    </div>
+  );
 }
