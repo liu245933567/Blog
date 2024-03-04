353526eef2f9c4d192d655493a1fb151727a42fc
Lenin <41979955+liu245933567@users.noreply.github.com>
2024-03-04 14:24:24 +0800

    ✨ feat: 添加博客文件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,12 +1,26 @@
 import { defineUserConfig } from "vuepress";
 import { recoTheme } from "vuepress-theme-reco";
+import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
-  theme: recoTheme({
-    // options
-  }),
   lang: "zh-CN",
-  title: "紫禁之巅攻略",
-  description: "你好， 欢迎查看紫禁之巅攻略~",
-  head: [["link", { rel: "icon", href: "https://wd.gyyx.cn/favicon.ico" }]],
+  title: "山有扶苏的博客",
+  description: "你好， 欢迎查看山有扶苏的博客~",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "https://s2.loli.net/2024/03/04/NpGLzUyWm7FkX1u.png",
+      },
+    ],
+  ],
+  bundler: viteBundler({
+    viteOptions: {},
+    vuePluginOptions: {},
+  }),
+  theme: recoTheme({
+    logo: "https://s2.loli.net/2024/03/04/NpGLzUyWm7FkX1u.png",
+    navbar: [{ text: "博客", link: "/posts", icon: "DocumentAttachment" }],
+  }),
 });
